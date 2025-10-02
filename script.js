dcdc3815ac7e208970171df9485c5c66d697178d
Shaman <118893857+Nurpeys@users.noreply.github.com>
2025-10-02 13:39:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["Engineer","Developer", "Designer", "Freelancer","TheAppWizard"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        cursorChar: ' |',
-        smartBackspace: true,
-        loop: true
-    });
+  // Typed.js animation
+var typed = new Typed(".typing", {
+  strings: ["Developer", "Freelancer", "Student", "App Creator"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
